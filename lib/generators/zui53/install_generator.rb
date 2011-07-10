bc327bddc4d7d8eaa0eac5a284e457c392a21b9c
Florian Günther <mail@gee-f.de>
2011-07-10 19:35:09 +0200

    ...

---
+++ lib/generators/zui53/install_generator.rb
@@ -0,0 +1,16 @@
+class InstallGenerator < Rails::Generators::Base
+  desc "Copies zui53 assets"
+  source_root File.expand_path('../../../assets', __FILE__)
+
+  def copy_stylesheets_and_images
+    ["zui53.js.coffee"].each do |f|
+      copy_file "javascripts/zui53/#{f}", "app/assets/javascripts/#{f}"
+    # copy_file "images/css3buttons/css3-github-buttons-icons.png", 
+    #           "public/images/css3buttons/css3-github-buttons-icons.png"
+    # copy_file "stylesheets/css3buttons/css3-github-buttons.css", 
+    #           "public/stylesheets/css3buttons/css3-github-buttons.css"
+    # copy_file "stylesheets/css3buttons/reset.css", 
+    #           "public/stylesheets/css3buttons/reset.css"
+    # gsub_file "public/stylesheets/css3buttons/css3-github-buttons.css", /url\(css3buttons/, "url(/images/css3buttons"
+  end
+end
--- lib/zui53.rb
+++ lib/zui53.rb
@@ -1,7 +1,9 @@
 module Zui53
-  if ::Rails.version >= "3.1"
+  # if ::Rails.version >= "3.1"
     class Engine < ::Rails::Engine
-
+      def self.hallo
+        puts "Hello World"
+      end
     end
-  end
+  # end
 end
